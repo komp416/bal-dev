823f8bc83e3c947aab40ba77184937ff0097bf85
komp416 <130561645+komp416@users.noreply.github.com>
2023-05-31 15:22:29 +0200

    BASIC CONTAINER. NO ERRORS

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    static: {
+        directory: path.join(__dirname, 'dist'),
+    },
     compress: true,
     port: 9000,
   },
